34c7cb4749d363344195fbdd3bc2daec52a10743
Shiburagi <tr32010@gmail.com>
2019-06-25 18:33:46 +0700

    Migrate to AndroidX

--- app/src/androidTest/java/com/infideap/drawerbehaviorexample/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/infideap/drawerbehaviorexample/ExampleInstrumentedTest.java
@@ -2,7 +2,7 @@ package com.infideap.drawerbehaviorexample;
 
 import android.content.Context;
 import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,7 +19,7 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getContext();
 
         assertEquals("com.infideap.drawerbehaviorexample", appContext.getPackageName());
     }
