f968d69153e8c4afaf6f6bb1f429861006d737e2
Jose Azcorra <jazcorra96@gmail.com>
2020-07-09 18:57:58 -0500

    Changed from background drawable to background color in order to keep the Card Effect

--- drawerbehavior/src/main/java/com/infideap/drawerbehavior/AdvanceDrawerLayout.java
+++ drawerbehavior/src/main/java/com/infideap/drawerbehavior/AdvanceDrawerLayout.java
@@ -7,7 +7,6 @@ import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.Gravity;
@@ -42,7 +41,7 @@ public class AdvanceDrawerLayout extends DrawerLayout {
     private int statusBarColor;
     private boolean defaultFitsSystemWindows;
     private float contrastThreshold = 3;
-    private Drawable cardBackground = null;
+    private int cardColor;
 
     public AdvanceDrawerLayout(Context context) {
         super(context);
@@ -65,7 +64,7 @@ public class AdvanceDrawerLayout extends DrawerLayout {
 
     private void init(Context context, AttributeSet attrs, int defStyle) {
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.advDrawerLayout);
-        cardBackground = a.getDrawable(R.styleable.advDrawerLayout_adl_cardBackground);
+        cardColor = a.getColor(R.styleable.advDrawerLayout_adl_cardBackgroundColor, Color.WHITE);
         a.recycle();
         defaultDrawerElevation = getDrawerElevation();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
@@ -151,12 +150,7 @@ public class AdvanceDrawerLayout extends DrawerLayout {
             cardView.setRadius(0);
             cardView.addView(child);
             cardView.setCardElevation(0);
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                cardView.setBackground(cardBackground);
-            }
-            else{
-                cardView.setBackgroundDrawable(cardBackground);
-            }
+            cardView.setCardBackgroundColor(cardColor);
             if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
                 cardView.setContentPadding(-6, -9, -6, -9);
             }
